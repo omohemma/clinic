4e6ef42aa7967ab5ac168223384cc4a03bc2049a
Omololu Emmanuel <emmaomoh@gmail.com>
2019-01-08 12:32:11 +0100

    oop db edited to server config

--- hcms/includes/db.php
+++ hcms/includes/db.php
@@ -5,7 +5,7 @@ class Database
 	public $conn;
 	function __construct()
 	{
-		$this->conn = mysqli_connect("localhost","root","","clinic");
+		$this->conn = mysqli_connect("us-cdbr-iron-east-01.cleardb.net","b55dc263b6abad","4056a8d5","heroku_6ce1e53c155f864");
 		
 		/*if($this->conn){
 			echo "Connected";
