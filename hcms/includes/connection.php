b5fb0e62072d093ab28c6bf079c1d346f3b4215e
Omololu Emmanuel <emmaomoh@gmail.com>
2019-01-08 02:47:40 +0100

    Connection Config

--- hcms/includes/connection.php
+++ hcms/includes/connection.php
@@ -1,9 +1,9 @@
 <?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "clinic";
+//mysql://b55dc263b6abad:4056a8d5@us-cdbr-iron-east-01.cleardb.net/heroku_6ce1e53c155f864?reconnect=true
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b55dc263b6abad";
+$password = "4056a8d5";
+$db = "heroku_6ce1e53c155f864";
 
 $conn = mysqli_connect($servername,$username,$password,$db);
 
